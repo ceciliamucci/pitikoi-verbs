a27e20e83cd5142b0680460850feafd9ddf1a479
ceciliamucci <cecilia_mucci@hotmail.com>
2026-06-15 21:15:27 -0300

    Force title and favicon refresh

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,26 @@
   import App from "./app/App.jsx";
   import "./styles/index.css";
 
+  const siteTitle = "Irregular verbs - Pitikoi";
+  const iconPath = "/koi-icon.png?v=2";
+
+  document.title = siteTitle;
+
+  let icon = document.querySelector("link[rel='icon']");
+  if (!icon) {
+    icon = document.createElement("link");
+    icon.rel = "icon";
+    document.head.appendChild(icon);
+  }
+  icon.type = "image/png";
+  icon.href = iconPath;
+
+  let appleIcon = document.querySelector("link[rel='apple-touch-icon']");
+  if (!appleIcon) {
+    appleIcon = document.createElement("link");
+    appleIcon.rel = "apple-touch-icon";
+    document.head.appendChild(appleIcon);
+  }
+  appleIcon.href = iconPath;
+
   createRoot(document.getElementById("root")).render(<App />);
